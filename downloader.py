071654b3a55ca3ed5c7efc32d64e440c3a9a08a7
O48WRX <kardyy81@gmail.com>
2026-02-24 22:42:25 +0100

    Debug branch created. ffmpeg path is broken. Have to fix

--- downloader.py
+++ downloader.py
@@ -1,15 +1,29 @@
 import yt_dlp
 import requests
+import os, sys
+
+def getBaseDir(): #Have to add this in case basedir breaks during packaging.
+    if getattr(sys, 'frozen', False):
+        return sys._MEIPASS
+    return os.path.dirname(os.path.abspath(__file__))
 
 def downloadVideo(url):
+    base_dir = getBaseDir()
+    ffmpeg_path = os.path.join(base_dir, "bin", "ffmpeg")
+    print("Using ffmpeg at:", ffmpeg_path)
+    print("Exists:", os.path.exists(ffmpeg_path))
     ydlOpts = {
         'format': 'bestvideo+bestaudio/best',
         'outtmpl': '%(title)s.%(ext)s',
+        'ffmpeg_location': ffmpeg_path,
     }
-    with yt_dlp.YoutubeDL(ydlOpts) as ydl:
-        ydl.download([url])
+    try:
+        with yt_dlp.YoutubeDL(ydlOpts) as ydl:
+            ydl.download([url])
+    except Exception as e:
+        print("Error:", e)
 
-def isVideoAvailable(url):
+def isVideoAvailable(url): #Checks video availability based on returned status code
     r = requests.get(url)
     if("Video unavailable" in r.text): return False
     match r.status_code:
